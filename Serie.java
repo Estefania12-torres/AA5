55c52e1e5a1c24ffa23a7e27d95ceac2ff679dad
Darwin Andrés Sarango Soto <132624867+DarwinSarango@users.noreply.github.com>
2023-06-12 18:17:26 -0500

    Update Serie.java
    Ciclo para la posición del Fibonacci

--- Serie.java
+++ Serie.java
@@ -9,3 +9,17 @@ public class Serie {
   public Serie() {
      this.valorList = new HashMap<>();
 }
+public long getFibonacci(int posicion) {
+            if (posicion == 0 || posicion == 1) {
+                return 1;
+            } else {
+                if (valorList.containsKey(posicion)) {
+                    return valorList.get(posicion);
+                }
+
+                long resultado = getFibonacci(posicion - 1) + getFibonacci(posicion - 2);
+                valorList.put(posicion, resultado);
+
+                return resultado;
+            }
+        }
