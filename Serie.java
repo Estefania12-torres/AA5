b841ed640807dd886f29fa7ced1494944c4d3243
Darwin Andrés Sarango Soto <132624867+DarwinSarango@users.noreply.github.com>
2023-06-12 18:14:54 -0500

    Update Serie.java
    El HashMap se implementa en el código para designar claves para los valores correspondientes que se pueden recuperar en cualquier punto dado.

--- Serie.java
+++ Serie.java
@@ -1,2 +1,11 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+
 public class Serie {
+  private Map<Integer, Long> valorList;
+  
+  public Serie() {
+     this.valorList = new HashMap<>();
 }
