5b31097747c9182f181f1e11a12622cef9ae4354
Estefania12-torres <133797333+Estefania12-torres@users.noreply.github.com>
2023-06-12 19:18:20 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,26 @@
-
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+import java.util.Map;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Serie serie = new Serie();
+        
+        // Ejemplo: Obtener el valor de Fibonacci en la posición 50
+        int posicion = 50;
+        long fibonacci = serie.getFibonacci(posicion);
+        
+        // Imprimir directamente el HashMap
+        Map<Integer, Long> valorList = serie.getValorList();
+        System.out.println(valorList);
+        
+        // Utilizar el Scanner para obtener la posición de Fibonacci desde el usuario
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese la posición de Fibonacci: ");
+        posicion = scanner.nextInt();
+        fibonacci = serie.getFibonacci(posicion);
+        System.out.println("El valor de Fibonacci en la posición " + posicion + " es: " + fibonacci);
+        scanner.close();   
+      
+    }
+}
