050086dec982f8cdcaed5d465358c6e498f2429b
Estefania12-torres <133797333+Estefania12-torres@users.noreply.github.com>
2023-06-12 19:09:34 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,26 +1,10 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,s
 // then press Enter. You can now see whitespace characters in your code.
-import java.util.Map;
-import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+
         Serie serie = new Serie();
-        
-        // Ejemplo: Obtener el valor de Fibonacci en la posición 50
-        int posicion = 50;
-        long fibonacci = serie.getFibonacci(posicion);
-        
-        // Imprimir directamente el HashMap
-        Map<Integer, Long> valorList = serie.getValorList();
-        System.out.println(valorList);
-        
-        // Utilizar el Scanner para obtener la posición de Fibonacci desde el usuario
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Ingrese la posición de Fibonacci: ");
-        posicion = scanner.nextInt();
-        fibonacci = serie.getFibonacci(posicion);
-        System.out.println("El valor de Fibonacci en la posición " + posicion + " es: " + fibonacci);
-        scanner.close();   
-      
+        System.out.println(serie.getFibonacci(50));
+        System.out.println(serie);
     }
 }
--- Serie.java
+++ Serie.java
@@ -1,48 +1,52 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
 
+import java.util.*;
 
 public class Serie {
-  
-  private Map<Integer, Long> valorList;
-  
-  public Serie() {
-     this.valorList = new HashMap<>();
-}
-public long getFibonacci(int posicion) {
-            if (posicion == 0 || posicion == 1) {
-                return 1;
-            } else {
-                if (valorList.containsKey(posicion)) {
-                    return valorList.get(posicion);
-                }
 
-                long resultado = getFibonacci(posicion - 1) + getFibonacci(posicion - 2);
-                valorList.put(posicion, resultado);
+    private Map<Integer, Long> valorList;
 
-                return resultado;
-            }
-        }
-public void calcularFibonacciConScanner(){  // Esto define un método público llamado calcularFibonacciConScanner que no devuelve ningún valor (void).
-    Scanner scanner= new Scanner(System.in); //Crea una instancia de la clase Scanner llamada scanner y la inicializa para leer la entrada del usuario desde la consola (System.in).
-    System.out.print("Ingrese la posicion de Fibonacci:"); //Imprime el mensaje "Ingrese la posición de Fibonacci:" en la consola sin una nueva línea.
-    int posicion=scanner.nextInt(); //Lee un número entero ingresado por el usuario utilizando el objeto scanner y lo asigna a la variable posicion
-    long fibonacci=getFibonacci(posicion); //Llama a un método llamado getFibonacci pasando la variable posicion como argumento y asigna el resultado a la variable fibonacci
-    System.out.println("El valor de Fibonacci en la posicion"+ posicion+"es:"+ fibonacci); //: Imprime en la consola el mensaje. La función println agrega una nueva línea al final.
-    scanner.close(); //Cierra el objeto scanner para liberar los recursos asociados a la entrada del usuario.
-}
-  public Map<Integer, Long> getValorList() { // es un método de acceso (getter) para obtener el campo valorList. 
-            return valorList; //Devuelve un Map<Integer, Long>, que es el tipo de dato especificado en la declaración de la clase.
-        }
-  
-        public void setValorList(Map<Integer, Long> valorList) { //es un método de modificación (setter) que establece el campo valorList con el valor pasado como argumento.
-            this.valorList = valorList; // El argumento es un Map<Integer, Long>. El uso de this.valorList se refiere al campo de la clase, mientras que valorList sin this se refiere al parámetro del método.
-        }
 
-        @Override
-        public String toString() { // es una anulación del método toString() de la clase base Object
-            return "Serie{valorList=" + valorList + '}';  //Devuelve una representación en forma de cadena de la instancia de la clase Serie
-        }
+    public Serie() {
+        this.valorList = new HashMap<>();
     }
-  
+
+    public long getFibonacci(int position) {
+        if (position == 0 || position == 1) {
+            return 1;
+        } else {
+            if (valorList.containsKey(position)) {
+                return valorList.get(position);
+            }
+            long resultado = getFibonacci(position - 1) + getFibonacci(position - 2);
+            valorList.put(position, resultado);
+            return resultado;
+            }
+
+        }
+
+    public Map<Integer, Long> getValorList() {
+        return valorList;
+    }
+
+    public void setValorList(Map<Integer,Long>valorList){
+        this.valorList=valorList;
+    }
+    private void printValorList() {
+        StringBuilder builder = new StringBuilder();
+        for (Map.Entry<Integer, Long> entry : valorList.entrySet()) {
+            if (builder.length() > 0) {
+                builder.append(" , ");
+            }
+            builder.append(entry.getValue());
+        }
+        System.out.println(builder.toString());
+    }
+
+    public String toString() {
+        return "Serie: " + valorList.toString();
+    }
+
+}
+
+
+
